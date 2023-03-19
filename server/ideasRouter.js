35c9d41ce651abc58f73fc193da748f607dc69d2
Liam Bowie <liambowie16@gmail.com>
2023-03-19 13:15:55 +0000

    Implement ideas routes to get one or all ideas, post a new idea to the db, edit an existing idea, or delete an idea

---
+++ server/ideasRouter.js
@@ -0,0 +1,43 @@
+const db = require('./db');
+const express = require('express');
+const ideasRouter = express.Router();
+
+ideasRouter.param('ideaId', (req, res, next, id) => {
+    const idea = db.getFromDatabaseById('ideas', id);
+    if(idea) {
+        req.idea = idea;
+        next();
+    } else {
+        res.status(404).send();
+    }
+});
+
+ideasRouter.get('/', (req, res, next) => {
+    res.send(db.getAllFromDatabase('ideas'));
+});
+
+ideasRouter.post('/', (req, res, next) => {
+    const newIdea = db.addToDatabase('ideas', req.body);
+    res.status(201).send(newIdea);
+});
+
+ideasRouter.get('/:ideaId', (req, res, next) => {
+    res.send(req.idea);
+});
+
+ideasRouter.put('/:ideaId', (req, res, next) => {
+    let updatedIdeaInstance = db.updateInstanceInDatabase('ideas', req.body);
+    res.send(updatedIdeaInstance);
+});
+
+ideasRouter.delete('/:ideaId', (req, res, next) => {
+    const deleted = db.deleteFromDatabasebyId('ideas', req.idea.id);
+    if (deleted) {
+      res.status(204);
+    } else {
+      res.status(500);
+    }
+    res.send();
+});
+
+module.exports = ideasRouter;
