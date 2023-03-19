bb586afc7c68bd77f7d79094708853e64225e403
Liam Bowie <liambowie16@gmail.com>
2023-03-19 00:52:33 +0000

    Complete required roots for /api/minions

--- server/minionsRouter.js
+++ server/minionsRouter.js
@@ -2,8 +2,42 @@ const db = require('./db');
 const express = require('express');
 const minionsRouter = express.Router();
 
+minionsRouter.param('minionId', (req, res, next, id) => {
+    const minion = db.getFromDatabaseById('minions', id);
+    if(minion) {
+        req.minion = minion;
+        next();
+    } else { 
+        res.status(404).send();
+    }
+});
+
 minionsRouter.get('/', (req, res, next) => {
     res.send(db.getAllFromDatabase('minions'));
+});
+
+minionsRouter.post('/', (req, res, next) => {
+    const newMinion = db.addToDatabase('minions', req.body);
+    res.status(201).send(newMinion);
+});
+
+minionsRouter.get('/minionId', (req, res, next) => {
+    res.send(req.minion);
+});
+
+minionsRouter.put('/:minionId', (req, res, next) => {
+    let updatedMinionInstance = updateInstanceInDatabase('minions', req.body);
+    res.send(updatedMinionInstance);
+});
+
+minionsRouter.delete('/:minionId', (req, res, next) => {
+    const deleted = deleteFromDatabasebyId('minions', req.params.minionId);
+    if (deleted) {
+      res.status(204);
+    } else {
+      res.status(500);
+    }
+    res.send();
 });
 
 module.exports = minionsRouter;
